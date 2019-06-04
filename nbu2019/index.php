c4ebdd8078883b7082a794c68391118784941c24
timmyru <33581137+timmyru@users.noreply.github.com>
2019-06-04 15:27:08 +0300

    Add files via upload

---
+++ nbu2019/index.php
@@ -0,0 +1,151 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Тестовое задание | Прыгин Артем</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+<!--Лого и заголовки-->
+<div class="logo">
+    <img src="img/logo.jpg" alt="logo">
+    <h1 class="logo-title">НОВЫЙ БИЗНЕС УНИВЕРСИТЕТ</h1>
+    <h3 class="logo-title__second">Международный Центр Дистанционного Образования</h3>
+</div>
+
+<!--Форма-->
+<div class="form-block">
+    <div class="form-title">Полный курс кулинарного дела</div>
+    <div class="form-title form-title__second">КУЛИНАР - ЭКСПЕРТ</div>
+    <div class="form-descr">Повар 6-го разряда</div>
+    <div class="form-img">
+        <img src="img/cook.jpg" alt="cook">
+    </div>
+
+    <form action="#" class="form">
+        <input class="form-input" type="text" placeholder="Имя" name="name">
+        <input class="form-input" type="email" placeholder="Email" name="email">
+
+        <input class="form-checkbox" type="checkbox" name="checkbox" id="checkbox">
+        <label class="form-label" for="checkbox">Согласие на обработку персональных данных</label>
+    </form>
+
+    <div class="form-bottom">
+        <span class="form-bottom__top">Начать обучение бесплатно</span>
+        <span class="form-bottom__second">и посмотреть свой будущий диплом</span>
+        <span class="form-bottom__date">сегодня 25.04.2019</span>
+    </div>
+</div>
+
+<!--Получи диплом-->
+<div class="diploma section container">
+    <div class="title diploma-title">Получи официальный гос. Диплом с международным приложением в кратчайшие сроки</div>
+    <div class="descr diploma-descr">Этот содержательный курс даст вам именно те знания, которые вы ищете. Он поможет вам обрести уверенность и стать более компетентным в своей сфере. Не откладывайте ваше развитие и профессиональный рост на потом - запишитесь на курс</div>
+
+    <div class="diploma-license">
+        <div class="diploma-license__top">
+            <img src="img/diploma1.jpg" alt="diploma">
+            <span class="diploma-license__span">Лицензия на образовательную деятельность №038379</span>
+        </div>
+
+        <div class="diploma-license__middle">
+            <div class="diploma-license__middle-img">
+                <img src="img/diploma2.jpg" alt="diploma">
+                <img src="img/diploma3.jpg" alt="diploma">
+                <img src="img/diploma4.jpg" alt="diploma">
+            </div>
+            <span class="diploma-license__span">Лицензия на образовательную деятельность №038379</span>
+        </div>
+
+        <div class="diploma-license__bottom">
+            <img src="img/diploma5.jpg" alt="diploma">
+            <span class="diploma-license__span">Лицензия на образовательную деятельность №038379</span>
+        </div>
+    </div>
+</div>
+
+<!--Осваивай новые знания-->
+<div class="knowledge section container">
+    <div class="title knowledge-title">Осваивай новые знания быстрее и проще на современной СЛАЙД платформе обучения</div>
+    <div class="descr knowledge-descr">Этот содержательный курс даст вам именно те знания, которые вы ищете. Он поможет вам обрести уверенность и стать более компетентным в своей сфере. Не откладывайте ваше развитие и профессиональный рост на потом - запишитесь на курс</div>
+
+    <div class="knowledge-img">
+        <img src="img/course1.jpg" alt="diploma">
+        <img src="img/course2.jpg" alt="diploma">
+        <img src="img/course3.jpg" alt="diploma">
+    </div>
+
+    <div class="descr knowledge-descr">Этот содержательный курс даст вам именно те знания, которые вы ищете. Он поможет вам обрести уверенность и стать более компетентным в своей сфере. Не откладывайте ваше развитие и профессиональный рост на потом - запишитесь на курс</div>
+</div>
+
+<!--Форма-->
+<div class="form-block">
+    <div class="form-title">Полный курс кулинарного дела</div>
+    <div class="form-title form-title__second">КУЛИНАР - ЭКСПЕРТ</div>
+    <div class="form-descr">Повар 6-го разряда</div>
+    <div class="form-img">
+        <img src="img/cook.jpg" alt="cook">
+    </div>
+
+    <form action="#" class="form">
+        <input class="form-input" type="text" placeholder="Имя" name="name">
+        <input class="form-input" type="email" placeholder="Email" name="email">
+
+        <input class="form-checkbox" type="checkbox" name="checkbox" id="checkbox">
+        <label class="form-label" for="checkbox">Согласие на обработку персональных данных</label>
+    </form>
+
+    <div class="form-bottom">
+        <span class="form-bottom__top">Начать обучение бесплатно</span>
+        <span class="form-bottom__second">и посмотреть свой будущий диплом</span>
+        <span class="form-bottom__date">сегодня 25.04.2019</span>
+    </div>
+</div>
+
+<!--Иконки-->
+<div class="icons">
+    <div class="icon">
+        <img src="img/fb.svg" alt="social">
+    </div>
+    <div class="icon">
+        <img src="img/vk.svg" alt="social">
+    </div>
+    <div class="icon">
+        <img src="img/ok.svg" alt="social">
+    </div>
+    <div class="icon">
+        <img src="img/twitter.svg" alt="social">
+    </div>
+    <div class="icon">
+        <img src="img/linkedin.svg" alt="social">
+    </div>
+    <div class="icon">
+        <img src="img/pinterest.svg" alt="social">
+    </div>
+    <div class="icon">
+        <img src="img/fb.svg" alt="social">
+    </div>
+    <div class="icon">
+        <img src="img/vk.svg" alt="social">
+    </div>
+</div>
+
+<!--Футер-->
+<footer class="footer">
+    <ul class="footer-list">
+        <li><a href="#">ВОЙТИ В ЛИЧНЫЙ КАБИНЕТ</a></li>
+        <li><a href="#">УЗНАТЬ БОЛЬШЕ ОБ НБУ</a></li>
+        <li><a href="#">ВЫБРАТЬ СФЕРУ ОБУЧЕНИЯ</a></li>
+        <li><a href="#">УЗНАТЬ СТОИМОСТЬ</a></li>
+        <li><a href="#">КОНТАКТЫ</a></li>
+    </ul>
+    <div class="footer-descr">Профессиональные курсы с 2001 года</div>
+</footer>
+
+</body>
+</html>
