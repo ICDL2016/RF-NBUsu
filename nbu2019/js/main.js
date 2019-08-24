6f26a4984e96dd860b790a261628c96a2756d7de
timmyru <33581137+timmyru@users.noreply.github.com>
2019-08-24 22:07:46 +0300

    Add files via upload

--- nbu2019/js/main.js
+++ nbu2019/js/main.js
@@ -91,3 +91,11 @@ $('.fancybox-buttons').fancybox({
         this.title = 'Image ' + (this.index + 1) + ' of ' + this.group.length + (this.title ? ' - ' + this.title : '');
     }
 });
+
+
+// бургер меню
+
+let burger = document.querySelector('.personal-burger');
+burger.addEventListener('click', function () {
+    $('.personal-menu').slideToggle();
+});
